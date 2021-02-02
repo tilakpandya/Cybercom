8f94c8618c7119a25391ab5f554e6419234e0e72
tilakpandya <tilakpandya2@gmail.com>
2021-02-02 19:37:41 +0530

    02-02-2021 commit

---
+++ 02-02-2021/js/form1.js
@@ -0,0 +1,15 @@
+/* function validateForm() {
+  var name = document.myForm.name.value;
+  var pass = document.forms["myForm"]["password"].value;
+  var add = document.forms["myForm"]["address"].value;
+  var game = document.forms["myForm"]["game"].value;
+  var gender = document.forms["myForm"]["gender"].value;
+  var age = document.forms["myForm"]["age"].value;
+
+  if (name == "") {
+    alert("Please Enter your name:");
+    name.focus();
+    return false;
+  }
+}
+ */
